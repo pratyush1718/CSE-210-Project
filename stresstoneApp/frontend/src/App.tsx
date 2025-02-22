cb198be03718b52900b027c20e682fb981b69532
Jishnu Raychaudhuri <jishnuraychaudhuri@Jishnus-MacBook-Pro.local>
2025-02-22 15:11:41 -0800

    created path to explore page

--- stresstoneApp/frontend/src/App.tsx
+++ stresstoneApp/frontend/src/App.tsx
@@ -1,12 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home'; // create this component
 import Explore from './pages/Explore'; 
-// import Playlist from './pages/Playlist'; // create this component
-// import Discuss from './pages/Discuss'; // create this component
-// import Create from './pages/Create'; // create this component
-// import Upload from './pages/Upload'; // create this component
 import Header from './components/Header';
 import Sider from './components/Sider';
 import { Box, CssBaseline } from '@mui/material';
@@ -30,24 +25,28 @@ function App() {
     <>
       <CssBaseline />
       <Header height={headerHeight} />
-      <Box sx={{ display: 'flex', marginTop: `${headerHeight}px` }}>
+      <Box sx={{ display: "flex", marginTop: `${headerHeight}px` }}>
         <Sider drawerWidth={siderWidth} />
         <Box
           component="main"
           sx={{
             p: 2,
             width: `calc(100% - ${siderWidth}px)`,
-            height: '100%',
-            overflow: 'auto',
+            height: "100%",
+            overflow: "auto",
           }}
         >
           <Routes>
+            <Route path="/" element={
+              <>
+                {/* Existing login and welcome message */}
+                <Login />
+                <p>Stresstone App</p>
+                <p>{message}</p>
+              </>
+            } />
             <Route path="/explore" element={<Explore />} />
           </Routes>
-          {/* All module implementation should go here */}
-          <Login></Login>
-          <p>Stresstone App</p>
-          <p>{message}</p>
         </Box>
       </Box>
       <footer>
