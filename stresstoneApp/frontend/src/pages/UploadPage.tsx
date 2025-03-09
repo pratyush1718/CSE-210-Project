63b32feb8b84941e2f71339ba898c332d2feea08
aaron <aaron.lhh0401@gmail.com>
2025-03-08 17:14:14 -0800

    organize api call to dispatcher

--- stresstoneApp/frontend/src/pages/UploadPage.tsx
+++ stresstoneApp/frontend/src/pages/UploadPage.tsx
@@ -80,27 +80,10 @@ const UploadPage: React.FC = () => {
         formData.append("imageFile", imageFile);
     }
 
-    try {
-        const port = import.meta.env.VITE_BACKEND_PORT || 3000;
-        const response = await fetch(`http://localhost:${port}/api/upload/`, {
-          method: "POST",
-          body: formData,
-        });
-    
-        const data = await response.json();
-        if (data.success) {
-          alert("Your music file has been uploaded successfully!");
-          clear();
-          // console.log("Title:", title);
-          // console.log("Tags:", JSON.stringify(tags));
-          // console.log("Audio File:", audioFile.name);
-          window.location.reload();
-        } else {
-          alert("Upload failed: " + data.error);
-        }
-    } catch (error) {
-        console.error("Upload Failed:", error);
-        alert("Upload failed! Please try again.");
+    const uploadSuccess = await uploadDispatcher(formData);
+    if (uploadSuccess) {
+        clear();
+        window.location.reload();
     }
   };
   
