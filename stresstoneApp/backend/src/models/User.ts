ac91c5a57b5f1e4a27e73a06655d2543a6103a27
Jishnu Raychaudhuri <jishnuraychaudhuri@Jishnus-MacBook-Pro.local>
2025-03-09 14:58:58 -0700

    feat (register): username is now required and gets stored on mongoDB

--- stresstoneApp/backend/src/models/User.ts
+++ stresstoneApp/backend/src/models/User.ts
@@ -3,6 +3,7 @@ import mongoose, {Schema, Document} from "mongoose"
 interface IUser extends Document {
     firebaseId: string; 
     email: string;
+    username: string; 
     tags: { tag: string, count: number }[];
 }
 
@@ -14,6 +15,7 @@ const tagSchema = new Schema({
 const userSchema: Schema<IUser> = new Schema({
     firebaseId: {type: String, required: true},
     email: {type: String, required: true},
+    username: {type: String, required: true},
     tags: {type: [tagSchema], default: []},
 });
 
