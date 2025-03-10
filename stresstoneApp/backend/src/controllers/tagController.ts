fd5d1f3d07057e5de4df889cef1d4565aab064ac
AndrewJLung <104866953+AndrewJLung@users.noreply.github.com>
2025-03-09 18:10:04 -0700

    Liking or unliking a song will reflect on the backend and update user tags to keep recommendations relevant.

--- stresstoneApp/backend/src/controllers/tagController.ts
+++ stresstoneApp/backend/src/controllers/tagController.ts
@@ -1,21 +1,58 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 import User from "../models/User";
-import Content from "../models/Content";
+import SoundTrack from "../models/SoundTrack";
+import Like from "../models/Like";
  
 export const addTagsToUser = async(req: Request, res: Response) => {
-    const { firebaseId, title } = req.body;
-    
+    const { firebaseId, trackId } = req.body;
+
+    //console.log(firebaseId);
+    //console.log(trackId);
+
     try {
-        const content = await Content.findOne({title: title});
+        const content = await SoundTrack.findOne({_id: trackId});
+
+        const existingLike = await Like.findOne({
+            firebaseId,
+            soundtrackId: new mongoose.Types.ObjectId(trackId)
+        });
+
+        // since this script is directly running after the like func
+        // if a like doesn't exist, it was literally just unliked
+
+        // if a like does exist, it was literally just liked
+        
+        //console.log(existingLike);
+
         if (!content) {
             res.status(404).send("Content not found.");
             return;
         }
+
         const user = await User.findOne({firebaseId : firebaseId});
+
+
         if (!user) {
             res.status(404).send("User not found.");
             return;
         }
+
+        if (!existingLike) {
+            // this was literally just unliked
+            content.tags.forEach((tag: string) => {
+                const tagExists = user.tags.find((userTag) => userTag.tag === tag);
+                if (tagExists) {
+                    tagExists.count -= 1;
+                }
+            });
+            user.tags = user.tags.filter(tag => tag.count > 0);
+            console.log(user);
+            await user.save();
+            res.status(200).send("User unliked song. Tags updated successfully.");
+            return;
+        }
+
         content.tags.forEach((tag: string) => {
             const tagExists = user.tags.find((userTag) => userTag.tag === tag);
             if (tagExists) {
@@ -25,7 +62,11 @@ export const addTagsToUser = async(req: Request, res: Response) => {
                 user.tags.push({tag, count: 1});
             }
         });
+
+        console.log(user);
         await user.save();
+        res.status(200).send("User tags pushed successfully.");
+        return;
     }
     catch(error) {
         console.error(error);
